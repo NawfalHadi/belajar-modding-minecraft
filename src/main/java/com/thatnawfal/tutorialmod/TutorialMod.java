0e38bfc881205026c013c81f924a6be0b38562c2
NawfalHadi <nawfalhdi@gmail.com>
2024-11-22 22:00:22 +0800

    block_custom : adding two garnet custom block

--- src/main/java/com/thatnawfal/tutorialmod/TutorialMod.java
+++ src/main/java/com/thatnawfal/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.thatnawfal.tutorialmod;
 
+import com.thatnawfal.tutorialmod.block.ModBlocks;
 import com.thatnawfal.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
